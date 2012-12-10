a33ae03c2c13191e8922cd661535cf6b7cd7f25b
Wouter Timmermans <protowouter@gmail.com>
2012-12-10 17:42:26 +0100

    Don't initialize on asset compilation

--- config/application.rb
+++ config/application.rb
@@ -53,6 +53,8 @@ module Inschrijfformulier
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    # Don't use the database on asset compilation time
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
